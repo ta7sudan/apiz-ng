4544368767f9f98fc916d8470479ffa69ded21d5
ta7sudan <ta7sudan@gmail.com>
2019-06-14 14:02:05 +0800

    fix(querystring): bug fixed

--- dist/apiz.umd.js
+++ dist/apiz.umd.js
@@ -382,7 +382,7 @@
 
 	const querystring = function (obj) {
 	  if (Object.prototype.toString.call(obj) === '[object Object]') {
-	    return Object.keys(obj).map(k => Array.isArray(obj[k]) ? obj[k].map(v => `${encodeURIComponent(k)}=${encodeURIComponent(v)}`).join('&') : `${encodeURIComponent(k)}=${encodeURIComponent(obj[k])}`).join('&');
+	    return Object.keys(obj).map(k => Array.isArray(obj[k]) ? obj[k].map(v => `${encodeURIComponent(k)}=${encodeURIComponent(v == null ? '' : v)}`).join('&') : `${encodeURIComponent(k)}=${encodeURIComponent(obj[k] == null ? '' : obj[k])}`).join('&');
 	  } else if (typeof obj === 'string') {
 	    return obj;
 	  } else {
